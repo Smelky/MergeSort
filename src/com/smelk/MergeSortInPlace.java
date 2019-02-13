5f03ceab3417b0fd0464096cec98f9dba0b9f024
Smelky <46086711+Smelky@users.noreply.github.com>
2019-02-13 17:09:18 +0200

    add

--- src/com/smelk/MergeSortInPlace.java
+++ src/com/smelk/MergeSortInPlace.java
@@ -48,12 +48,4 @@ public class MergeSortInPlace {
             System.out.print(A[i] + " ");
         System.out.println();
     }
-
-    public static void main(String[] args) {
-        int arr[] = {12, 11, 13, 5, 6, 7};
-        int arr_size = arr.length;
-
-        mergeSort(arr, 0, arr_size - 1);
-        printArray(arr, arr_size);
-    }
 }
